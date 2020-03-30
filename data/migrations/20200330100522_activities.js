f12a4e499a99cb08e66b9ccc49d86591156aee62
alexis villaraza <alexis.borja@gmail.com>
2020-03-30 10:38:51 -0700

    created migrations table

---
+++ data/migrations/20200330100522_activities.js
@@ -0,0 +1,36 @@
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('activities', tbl => {
+      tbl.increments();
+
+      tbl.string('name', 255).notNullable();
+
+      tbl.integer('points').notNullable();
+
+      tbl.date('date', 255).notNullable();
+
+      tbl.string('activity_type', 255).notNullable();
+
+      tbl.string('desc', 255);
+
+      tbl.string('ec_name', 255);
+
+      tbl.integer('ec_points');
+  })
+    .createTable('employees', tbl => {
+      tbl.increments();
+
+      tbl.string('name', 255).notNullable();
+
+      tbl.string('activity_id').unsigned().notNullable().references('id').inTable('activities').onDelete('CASCADE').onUpdate('CASCADE');
+
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('activities')
+  .dropTableIfExists('employyes')
+  
+};
