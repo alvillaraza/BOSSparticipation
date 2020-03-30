a1c2baa31b7da8d7a8718557b1b425f133733731
alexis villaraza <alexis.borja@gmail.com>
2020-03-30 10:46:24 -0700

    able to run seed by fixing bugs

--- data/seeds/001-activities.js
+++ data/seeds/001-activities.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   return knex('activities').insert([
-    {name: 'Romantasy_Cabaret', points: '5', date: '12/22/2018', activity_type: 'choreography', description: 'choreographed performance for RC NYE', ec_name:'choregrapher', ec_points: '3'}
+    {name: 'Romantasy_Cabaret', points: '5', date: '12/22/2018', activity_type: 'choreography', desc: 'choreographed performance for RC NYE', ec_name:'choregrapher', ec_points: '3'}
   ])
    
 };
