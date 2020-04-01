c6da2a6d1a1fbee0f3322e73da471b94af5e3c66
alexis villaraza <alexis.borja@gmail.com>
2020-04-01 10:43:05 -0700

    figured out seed files, removed seed user and seed employees

--- data/seeds/001-users.js
+++
@@ -1,7 +0,0 @@
-// exports.seed = function(knex) {
-//   return knex("users").insert([
-//     { name: "Kristin Travis", activity_id: 1, is_admin: false },
-//     { name: "Shirin Ahmadpour", activity_id: 2, is_admin: false },
-//     { name: "Christina Bui", activity_id: 3, is_admin: false }
-//   ]);
-// };
